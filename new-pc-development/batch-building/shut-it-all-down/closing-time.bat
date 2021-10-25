3c46a92fec2faecbe6990445d1887fe5f99ce096
GoodGuyGregory <greg.witt625@gmail.com>
2021-10-24 21:22:46 -0400

    adds format for closing programs

--- new-pc-development/batch-building/shut-it-all-down/closing-time.bat
+++ new-pc-development/batch-building/shut-it-all-down/closing-time.bat
@@ -1,5 +1,5 @@
 @echo off
 echo closing all open programs....
-@powershell -command "(New-Object -comObject Shell.Application).Windows() | foreach-object {$_.quit()}; Get-Process | Where-Object {$_.MainWindowTitle -ne \"\"} | stop-process"
 timeout 10
+@powershell -command "(New-Object -comObject Shell.Application).Windows() | foreach-object {$_.quit()}; Get-Process | Where-Object {$_.MainWindowTitle -ne \"\"} | stop-process"
 exit
